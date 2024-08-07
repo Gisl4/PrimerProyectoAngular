65424bbb87b0b3d5eb4bfb444ba558aff04f32de
Gisl4 <lopgiz25@gmail.com>
2024-08-07 02:11:36 +0200

    Pipes y Directivas

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  cities = ['Múnich', 'Düsseldorf', 'Leipzig', 'Stuttgart', 'Friburgo'];
+  showCity: boolean = true;
+  changeCss: boolean = true;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  show() {
+    this.showCity = !this.showCity;
+  }
+
+  exchangeCss() {
+    this.changeCss = !this.changeCss;
+  }
 
 }
